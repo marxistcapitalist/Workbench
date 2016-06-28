e44989a4d7feb0c4d37caac84af309c9e3cd398a
Ethan <webmaster@syrukide.net>
2016-06-28 15:27:57 -0400

    Fix homepage

--- project/src/main/resources/public/js/home.js
+++ project/src/main/resources/public/js/home.js
@@ -5,24 +5,24 @@
 */
 
 var workbench_debug = false;
-var workbench_launch_attempts = 0;
-var workbench_launch_timeout;
 var workbench_settings = {
   api: {
     uri: "http://workbench.online/api",
     port: "80"
   }
 };
+var workbench_launchtimeout;
+var workbench_launchattempts = 1;
 
 /* ===================================== */
 /* === STARTUP                       === */
 /* === Only used for loading         === */
 /* ===================================== */
-window.onload = function() {
+function workbench_startup() {
   if(window.jQuery) {
     if(workbench_debug)
       console.log("[Workbench] jQuery is loaded, starting...");
-    workbench_launch();
+    workbench_dependencies();
     return;
   } else {
     if(workbench_debug)
@@ -30,8 +30,21 @@ window.onload = function() {
     var jreplace = document.createElement("script");
     jreplace.src = "js/jquery-2.2.4.min.js";
     document.getElementsByTagName("head").appendChild(jreplace);
-    setTimeout(workbench_launch, 2000);
+    setTimeout(workbench_startup, 2000);
   }
+}
+
+function workbench_dependencies() {
+  // TODO failure handling
+  $.getScript("js/controllers/NotificationController.js");
+  $.getScript("js/controllers/RequestController.js");
+
+  $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js");
+  $.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js");
+  $.getScript("js/cookies.js");
+  $.getScript("js/jquery.mCustomScrollbar.concat.min.js");
+  $.getScript("js/jquery.scrollTo.min.js");
+  workbench_launch();
 }
 
 /* ===================================== */
@@ -39,26 +52,33 @@ window.onload = function() {
 /* === Start Workbench instance      === */
 /* ===================================== */
 function workbench_launch() {
-  /* === Include dependencies for testing user and bench validity === */
-  // TODO Failure handling
-  $.getScript("js/controllers/NotificationController.js");
-  $.getScript("js/controllers/RequestController.js");
-
-  $.getScript("js/cookies.js");
-
-  /* === Test for user login === */
-  wb_notificiation = new NotificationController();
-  wb_request = new RequestController(workbench_settings.api.uri, wb_notificiation);
-  if(docCookies.hasItem("workbench_userid") && docCookies.hasItem("workbench_token")) {
-    wb_request.send(wb_request.protocol.account.auth(), function(data) {
-      // TODO : Grab user data
-      wb_bench = new BenchController(wb_user, wb_notificiation, wb_request);
-    }, function(data) { // Fail
+  try {
+    wb_notificiation = new NotificationController();
+    wb_request = new RequestController(workbench_settings.api.uri, wb_notificiation);
+    if(docCookies.hasItem("workbench_userid") && docCookies.hasItem("workbench_token")) {
+      wb_request.send(wb_request.protocol.account.auth(), function(data) {
+        // TODO : Grab user data
+        wb_bench = new BenchController(wb_user, wb_notificiation, wb_request);
+      }, function(data) { // Fail
+        return;
+      });
+    } else {
       return;
-    });
-  } else {
-    return;
+    }
+  } catch(e) {
+    console.error(e.message);
+    if(workbench_launchattempts < 11) {
+      console.warn("[Workbench] Load attempt " + workbench_launchattempts + " failed, retrying in 1 second...");
+      clearTimeout(workbench_launchtimeout);
+      workbench_launchtimeout = setTimeout(workbench_launch, 1000);
+      workbench_launchattempts++;
+    } else {
+      clearTimeout(workbench_launchtimeout);
+      console.error("[Workbench] Failed to load after 10 tries.");
+      return;
+    }
   }
+}
 
   /* === Include Remaning Dependencies === */
   $.getScript("js/jquery.mCustomScrollbar.js");
@@ -133,3 +153,7 @@ var HomeController = function() {
     };
   }
 }
+
+// === STARTUP RAW JS
+window.onload = workbench_startup;
+document.ready = workbench_launch;
